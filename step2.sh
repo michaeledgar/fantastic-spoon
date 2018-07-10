cae45e6beb685d01ad4481e1be8589cc2f91ef7a
Mike Edgar <adgar@google.com>
2018-07-10 13:25:46 -0400

    print a lot more easters

--- step2.sh
+++ step2.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get -y install bsdmainutils
 
-for y in $(seq 1970 2070)
+for y in $(seq 1970 3070)
 do
   echo "Western Easter in ${y}: $(ncal -e ${y})"
   echo "Orthodox Easter in ${y}: $(ncal -o ${y})"
